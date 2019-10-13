217780f70dc6cd0f9a9a8d2bcea3028e091de05c
Your Name <you@example.com>
2019-10-13 17:19:41 +0700

    second commit

---
+++ data/recipes.js
@@ -0,0 +1,8 @@
+module.exports = [
+    { Air: { Air: 'Pressure', Earth: 'Dust', Fire: 'Energy', Water: 'Rain', Pressure: 'Atmosphere', Energy: 'Wind', Lava: 'Stone', Steam: 'Cloud' } },
+    { Earth: { Earth: 'Pressure', Fire: 'Lava', Water: 'Mud', Rain: 'Plain', Lava: 'Volcano' } },
+    { Fire: { Water: 'Steam', Plain: 'Tobacco', Mud: 'Brick' } },
+    { Water: { Water: 'Sea', Sea: 'Ocean', Lava: 'Obsidian' } },
+    { Pressure: { Lava: 'Granite', Plain: 'Coal' } },
+    { Dust: {} }, { Energy: {} }, { Rain: {} }, { Lava: {} }, { Mud: {} }, { Steam: {} }, { Sea: {} }, { Atmosphere: {} }, { Wind: {} }, { Stone: {} }, { Plain: {} }, { Cloud: {} }, { Ocean: {} }, { Obsidian: {} }, { Tobacco: {} }, { Brick: {} }, { Volcano: {} }, { Granite: {} }, { Coal: {} }
+]
